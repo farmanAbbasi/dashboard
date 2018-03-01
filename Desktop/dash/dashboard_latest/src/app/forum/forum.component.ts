70b1b136d80cb779a957810e7332f139e1013541
Mohammad Farman Abbasi <moabbasi@BLR.ALLEGISINDIA.COM>
2018-03-01 19:28:51 +0530

    updated the working of poll day 2

--- Desktop/dash/dashboard_latest/src/app/forum/forum.component.ts
+++ Desktop/dash/dashboard_latest/src/app/forum/forum.component.ts
@@ -41,55 +41,51 @@ export class ForumComponent implements OnInit {
   selected = 'post';
   flagForPost = true;
   flagForPoll = false;
-  arrayOption1='';
-  arrayOption2='';
-  arrayOption3='';
-  arrayOption4='';
-  optionsToShare=[];
-  ioption=0;
-  pollQuestion='';
-  pollShareFlag=false;
- 
-  o1=false;o2=false;o3=false;
-  k=1;
+  arrayOption1 = '';
+  arrayOption2 = '';
+  arrayOption3 = '';
+  arrayOption4 = '';
+  arrayOptionAll = [];
+  indexArrayOptionAll = 0;
+  optionsToShare = [];
+  ioption = 0;
+  pollQuestion = '';
+  pollShareFlag = false;
+
+  o1 = false; o2 = false; o3 = false;
+  k = 1;
 
   constructor() { }
 
-  addBtnClicked()
-  {
-     if(this.k<=3)
-     {
-       this.k=this.k-1;
-      if(this.k==0)
-      this.o1=true;
-      if(this.k==1)
-      this.o2=true;
-      if(this.k==2)
-      this.o3=true;
-      this.k+=2;
-     }
-  }
-   //after all 4 btns appear k will be equal to 2
-  minusBtnClicked()
-  {
-   if(this.k==4)
-    {
-      this.o3=false;
-      this.arrayOption4='';
+  addBtnClicked() {
+    if (this.k <= 3) {
+      this.k = this.k - 1;
+      if (this.k == 0)
+        this.o1 = true;
+      if (this.k == 1)
+        this.o2 = true;
+      if (this.k == 2)
+        this.o3 = true;
+      this.k += 2;
     }
-   if(this.k==3)
-     {
-       this.o2=false;
-       this.arrayOption3='';
-     }
-   if(this.k==2)
-     {
-       this.o1=false;
-       this.arrayOption2='';
-     }
-   this.k--;
   }
- 
+  //after all 4 btns appear k will be equal to 2
+  minusBtnClicked() {
+    if (this.k == 4) {
+      this.o3 = false;
+      this.arrayOption4 = '';
+    }
+    if (this.k == 3) {
+      this.o2 = false;
+      this.arrayOption3 = '';
+    }
+    if (this.k == 2) {
+      this.o1 = false;
+      this.arrayOption2 = '';
+    }
+    this.k--;
+  }
+
   selectChangeHandler(event: any) {
     this.selected = event.target.value;
     if (this.selected == "post") {
@@ -98,7 +94,7 @@ export class ForumComponent implements OnInit {
     } else if (this.selected == "poll") {
       this.flagForPoll = true;
       this.flagForPost = false;
-    } 
+    }
   }
 
   ngOnInit() {
@@ -107,10 +103,17 @@ export class ForumComponent implements OnInit {
     this.thought = '';
     // this.shared=true;
   }
+
   deletePollQuestion() {
     this.pollQuestion = '';
-    
+    /*
+     this.arrayOption1=''; 
+     this.arrayOption2='';
+     this.arrayOption3='';
+    this.arrayOption4='';
+    */
   }
+
   onThoughtKeyUp(event: any) {
     this.thought = event.target.value;
     console.log('firedThrow');
@@ -120,32 +123,42 @@ export class ForumComponent implements OnInit {
     console.log('firedpoll');
   }
 
-  onOption1KeyUp(event :any){
-     this.arrayOption1=event.target.value;
-  }
-  onOption2KeyUp(event :any){
-    this.arrayOption2=event.target.value;
-  }
-  onOption3KeyUp(event :any){
-    this.arrayOption3=event.target.value;
- }
- onOption4KeyUp(event :any){
-   this.arrayOption4=event.target.value;
- }
+  onOption1KeyUp(event: any) {
+    this.arrayOption1 = event.target.value;
+    console.log('typing option 1');
 
-   iii=0;
-  sharePoll(){
-    if(this.pollQuestion.length!==0)
-    {
-      this.pollShareFlag=true;
-      this.optionsToShare[this.iii]=this.pollQuestion; 
+  }
+  onOption2KeyUp(event: any) {
+    this.arrayOption2 = event.target.value;
+    console.log('typing option 2');
+
+  }
+  onOption3KeyUp(event: any) {
+    this.arrayOption3 = event.target.value;
+    console.log('typing option 3');
+
+  }
+  onOption4KeyUp(event: any) {
+    this.arrayOption4 = event.target.value;
+    console.log('typing option 4');
+
+  }
+
+  iii = 0;
+  newArrayForPoll = [][4];
+  sharePoll() {
+    if (this.pollQuestion.length !== 0  ) {
+     
+      this.optionsToShare[this.iii] = this.pollQuestion;
       this.iii++;
+      this.arrayOptionAll[(this.iii*4)+0] = this.arrayOption1;
+      this.arrayOptionAll[(this.iii*4)+1] = this.arrayOption2;
+      this.arrayOptionAll[(this.iii*4)+2] = this.arrayOption3;
+      this.arrayOptionAll[(this.iii*4)+3] = this.arrayOption4;
+      this.pollShareFlag = true;
       this.deletePollQuestion();
     }
-    
   }
-
-
   shareThought() {
     if (this.shared === true && this.thought.length !== 0) {
       this.toShare = true;
@@ -159,21 +172,12 @@ export class ForumComponent implements OnInit {
   }
   onScroll() {
     console.log('fired');
-
     if (this.noOfItemsToShowInitially <= dog.length) {
-
       this.noOfItemsToShowInitially += this.itemsToLoad;
-
       this.itemsToShow = dog.slice(0, this.noOfItemsToShowInitially);
-
       console.log('scrolled');
-
     } else {
-
       this.isFullListDisplayed = true;
-
     }
-
   }
-
 }
